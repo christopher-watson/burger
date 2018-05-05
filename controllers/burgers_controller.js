28b347ffab89ed905997322665780d97bfc293f5
Christopher Watson <christopher_watson@icloud.com>
2018-05-04 23:47:26 -0400

    handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,3 +4,38 @@ var router = express.Router();
 var burger = require("../models/burger");
 
 //routes
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var burgerObj = {
+      burgers: data
+    };
+    console.log(burgerObj);
+    res.render("index", burgerObj);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function(result) {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+  console.log("condition", condition);
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,44 @@
 var orm = require("../config/orm");
 
+$(function() {
+  $(".change-eaten").on("click", function(event) {
+    var id = $(this).data("id");
+    var nowEaten = $(this).data("nowEat");
+
+    var newEatenState = {
+      devoured: nowEaten
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newEatenState
+    }).then(
+      function() {
+        console.log("changed sleep to", nowEaten);
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#burger-input").val().trim(),
+      devoured: false
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        location.reload();
+      }
+    );
+  });
+});
 
 
 
